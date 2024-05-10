dcdafa536c4faecbfa6adb534abc05d7bdd3fd38
Ayush Chopra <86606259+BrownCoderAyush@users.noreply.github.com>
2024-05-10 17:54:46 +0530

    configured routes and pages

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import MainRoutes from './routes/MainRoutes'
+
+function App() {
+
+  return (
+    <>
+     <nav>Simple Nav</nav>
+    <MainRoutes/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+export default function Home(){
+    return (
+        <>
+         {/*navbar*/}
+         {/*Movie List*/}
+         {/*pagination pages*/}
+         Movie Page
+        </>
+    )
+}
