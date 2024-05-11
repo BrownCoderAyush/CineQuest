9f206c8f291096254bd2764731cfa23b1d7b36f1
Ayush Chopra <86606259+BrownCoderAyush@users.noreply.github.com>
2024-05-12 01:46:59 +0530

    added auto complete UI

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,27 @@
+import { useRef } from "react";
 import "./Navbar.css"
-function Navbar(){
+function Navbar() {
+    const resultListRef = useRef(null);
     return (
         <div className="navbar-wrapper">
             <div className="navbar-title">MovieBase</div>
             <div className="search-bar">
-                <input type="text" placeholder="what movie are you thinking about..." />
+                <input
+                    type="text"
+                    id="movie-search-input"
+                    onFocus={()=>{
+                        resultListRef.current.style.display='block';
+                    }}
+                    onBlur={()=>{
+                        resultListRef.current.style.display='none';
+                    }}
+                    placeholder="what movie are you thinking about..."
+                />
+                <div id="result-list" ref={resultListRef}>
+                    <div>result 1</div>
+                    <div>result 2</div>
+                    <div>result 3</div>
+                </div>
             </div>
             <div className="navbar-theme">
                 Theme
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,23 @@
-import MovieCard from "../components/MovieCard/MovieCard"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { searchMovie } from "../apis/omdb"
 import './Home.css'
 
-export default function Home(){
+//components import 
+import MovieCard from "../components/MovieCard/MovieCard"
+
+
+export default function Home() {
+
+    const [movieList, setMovieList] = useState([]);
+    async function downloadDefaultMovies(...args) {
+        const urls = args.map((searchTerm) => searchMovie(searchTerm));
+        const response = await axios.all(urls.map((url) =>
+            axios.get(url)
+        ))
+        let moviesList = response.map((movies) => movies.data.Search);
+        setMovieList([].concat(...moviesList));
+    }
     const movie = {
         "Title": "Harry Potter and the Deathly Hallows: Part 2",
         "Year": "2011",
@@ -11,20 +27,27 @@ export default function Home(){
         "Genre": "Adventure, Family, Fantasy",
         "Poster": "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
     }
+    useEffect(() => {
+        downloadDefaultMovies("harry", "avengers");
+    }, [])
+
+
     return (
         <>
-         {/*navbar*/}
-         {/*Movie List*/}
-         {/*pagination pages*/}
-         <div className="movie-card-wrapper">
-         <MovieCard {...movie}/>
-         <MovieCard {...movie}/>
-         <MovieCard {...movie}/>
-         <MovieCard {...movie}/>
-         <MovieCard {...movie}/>
-         <MovieCard {...movie}/>
-         <MovieCard {...movie}/>
-         </div>
+            {/*navbar*/}
+            {/*Movie List*/}
+            {/*pagination pages*/}
+            <div className="movie-card-wrapper">
+                {movieList.map((movie) => <MovieCard
+                    key={movie.imdbID}
+                    {...movie}
+                />
+                )
+                }
+                {/* <MovieCard {...movie}/>
+                <MovieCard {...movie}/>
+                <MovieCard {...movie}/> */}
+            </div>
         </>
     )
 }
