4786c24bd7537e9847b018ddca463344de7f49a1
Ayush Chopra <86606259+BrownCoderAyush@users.noreply.github.com>
2024-05-11 15:41:26 +0530

    completed with navbar and card component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,30 @@
+import MovieCard from "../components/MovieCard/MovieCard"
+import './Home.css'
+
 export default function Home(){
+    const movie = {
+        "Title": "Harry Potter and the Deathly Hallows: Part 2",
+        "Year": "2011",
+        "Rated": "PG-13",
+        "Released": "15 Jul 2011",
+        "Runtime": "130 min",
+        "Genre": "Adventure, Family, Fantasy",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg",
+    }
     return (
         <>
          {/*navbar*/}
          {/*Movie List*/}
          {/*pagination pages*/}
-         Movie Page
+         <div className="movie-card-wrapper">
+         <MovieCard {...movie}/>
+         <MovieCard {...movie}/>
+         <MovieCard {...movie}/>
+         <MovieCard {...movie}/>
+         <MovieCard {...movie}/>
+         <MovieCard {...movie}/>
+         <MovieCard {...movie}/>
+         </div>
         </>
     )
 }
